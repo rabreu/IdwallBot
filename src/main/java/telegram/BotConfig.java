eecfb45aa21b6ecca705cb99ad52683cb691e8a7
Renata Abreu <renata89abreu@gmail.com>
2020-12-08 12:33:42 -0300

    BOT_TOKEN and BOT_USERNAME is now env vars

--- src/main/java/telegram/BotConfig.java
+++ src/main/java/telegram/BotConfig.java
@@ -1,6 +1,6 @@
 package telegram;
 
 public class BotConfig {
-    public static final String BOT_TOKEN = "1316104718:AAF3Hg0RB26BGGjEOVWvG8oJhHyvshbGIH0";
-    public static final String BOT_USERNAME = "desafio_idwall_bot";
+    public static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
+    public static final String BOT_USERNAME = System.getenv("BOT_USERNAME");
 }
